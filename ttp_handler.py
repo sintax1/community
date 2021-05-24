93a275c62240ff01e2347ecdea9f0818cb283202
Alex <43142871+khyberspache@users.noreply.github.com>
2021-05-24 13:38:11 -0400

    Adding a Travis pipeline to handle TTP update actions (#119)
    * Adding a Travis pipeline to handle TTP update actions

---
+++ ttp_handler.py
@@ -0,0 +1,19 @@
+import subprocess
+from ruamel.yaml import YAML
+from datetime import datetime
+
+
+def update_ttp(filename):
+    yaml = YAML()
+    ttp = yaml.load(open(filename))
+    ttp['metadata']['release_date'] = datetime.utcnow().date()
+    yaml.dump(ttp, open(filename, 'w'))
+
+
+if __name__ == '__main__':
+    results = subprocess.run(['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD'], cwd='./', capture_output=True)
+    modified_files = results.stdout.decode('utf-8').rstrip().split('\n')
+    for file in modified_files:
+        print(file)
+        if file.endswith('.yml') and file != '.travis.yml':
+            update_ttp(filename=file)
