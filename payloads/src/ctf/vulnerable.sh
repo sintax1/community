659f32667fb6c4ca5750b46d2771598605f4d829
Bartimus <465079+bartimus-primed@users.noreply.github.com>
2022-02-10 10:51:04 -0500

    remove payloads subdirs and remove CTF/Protect training (#323)

--- payloads/src/ctf/vulnerable.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo groupadd bunny && sudo usermod -aG bunny ec2-user;
-sudo echo "console.aws.amazon.com, iamroot, th3mar3th3appl3s" >> /var/local/sample.csv;
-sudo useradd privateducky && sudo mkdir /home/privateducky/.contacts && sudo echo "d2lsbCBzbWl0aA==: 555-555-5555" >> /home/privateducky/.contacts/phone.txt && sudo echo "nobigdeal:)" | gpg -c --batch --yes --passphrase-fd 0 /home/privateducky/.contacts/phone.txt && sudo rm /home/privateducky/.contacts/phone.txt;
